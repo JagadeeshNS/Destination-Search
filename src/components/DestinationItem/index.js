ae4a3b6d2782cdf8ab9615baee75b405d0b4b864
JagadeeshNS <129928667+JagadeeshNS@users.noreply.github.com>
2023-05-11 21:03:37 +0530

    Update index.js

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,12 +1,14 @@
 // Write your code here
+import './index.css'
+
 const DestinationItem = props => {
   const {destinationDetails} = props
   const {imgUrl, name} = destinationDetails
 
   return (
-    <li className="">
-      <img src={imgUrl} className="" alt={name} />
-      <p className="">{name}</p>
+    <li className="list-item">
+      <img src={imgUrl} className="list-image" alt={name} />
+      <p className="list-paragraph">{name}</p>
     </li>
   )
 }
